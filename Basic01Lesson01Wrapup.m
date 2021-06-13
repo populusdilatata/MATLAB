a3968801b9ae38bb41b84feee6aba1bb7a410702
populusdilatata <tom.majda@seznam.cz>
2021-06-13 21:31:44 +0200

    Add files via upload
    Lesson01m-files

---
+++ Basic01Lesson01Wrapup.m
@@ -0,0 +1,6 @@
+%As of earlz 2018, Usain Bolt holds the world record in the men's 100-meter dash. Is is 9.58 seconds. What was his average speed in km/h?
+%Assign the result to a variable called hundred.
+hundred=(360/9.58)
+
+remains=(99/3600)
+marathon=42.195/(2+remains)
